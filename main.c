0dfb583f95037ed086511a45b9ff2c7e24ffea29
axis710K <heroldaksd@gmail.com>
2018-08-14 20:21:06 -0500

    easier readability
    added comments with more description than ever before

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main()
     //init hamlib
     ham_Init();
     
-    //init ham for text
+    //init hamlib for text display
     ham_InitText(0);
     
-    //display button names
+    //display button names onscreen
     ham_DrawText(0,0,"Button input test");
     ham_DrawText(3,2,"UP");
     ham_DrawText(3,3,"DOWN");
@@ -25,6 +25,7 @@ int main()
     ham_DrawText(3,10,"START");
     ham_DrawText(3,11,"SELECT");
     
+    //program loop
     while(1)
     {
         //check UP
@@ -32,47 +33,56 @@ int main()
             ham_DrawText(0,2,"X");
         else
             ham_DrawText(0,2," ");
-            
+
+        //check DOWN
         if(F_CTRLINPUT_DOWN_PRESSED)
             ham_DrawText(0,3,"X");
         else
             ham_DrawText(0,3," ");
-            
+
+        //check LEFT
         if(F_CTRLINPUT_LEFT_PRESSED)
             ham_DrawText(0,4,"X");
         else
             ham_DrawText(0,4," ");
 
+        //check RIGHT
         if(F_CTRLINPUT_RIGHT_PRESSED)
             ham_DrawText(0,5,"X");
         else
             ham_DrawText(0,5," ");
 
+        //check A button
         if(F_CTRLINPUT_A_PRESSED)
             ham_DrawText(0,6,"X");
         else
             ham_DrawText(0,6," ");
 
+        //check B button
         if(F_CTRLINPUT_B_PRESSED)
             ham_DrawText(0,7,"X");
         else
             ham_DrawText(0,7," ");
 
+        //check L trigger
         if(F_CTRLINPUT_L_PRESSED)
             ham_DrawText(0,8,"X");
         else
             ham_DrawText(0,8," ");
 
+        //check R trigger
         if(F_CTRLINPUT_R_PRESSED)
             ham_DrawText(0,9,"X");
         else
             ham_DrawText(0,9," ");
 
+        //check START button
         if(F_CTRLINPUT_START_PRESSED)
             ham_DrawText(0,10,"X");
         else
             ham_DrawText(0,10," ");
 
+        //check SELECT button
         if(F_CTRLINPUT_SELECT_PRESSED)
             ham_DrawText(0,11,"X");
         else
@@ -82,9 +92,5 @@ int main()
 //end program
 return 0;
 }
-            
-
-
-
 
 /* END OF FILE */
